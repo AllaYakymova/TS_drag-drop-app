3d2b3ce21d5c5bad1909fec6a2c1f4e9880f8c60
Allegra <allochkaja112@gmail.com>
2021-08-13 10:02:31 +0300

    ts created ProjectInput and decorator autobind

---
+++ src/app.ts
@@ -0,0 +1,54 @@
+
+// auto bind decorator
+function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      return originalMethod.bind(this);
+    }
+  };
+  return adjDescriptor;
+}
+
+// ProjectInput class
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hastElement: HTMLDivElement;
+  element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
+
+  constructor() {
+    this.templateElement = <HTMLTemplateElement>document.getElementById('project-input')!; // // the same option of type assignment
+    this.hastElement = document.getElementById('app')! as HTMLDivElement; // the same option of type assignment
+    const importedNode = document.importNode(this.templateElement.content, true);
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = 'user-input';
+
+    this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+
+    this.configure();
+    this.attach();
+  }
+
+  @autobind // help to make auto bind  this in addEventListener
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+
+  private configure() {
+    this.element.addEventListener('submit', this.submitHandler)
+  }
+
+  private attach() {
+    this.hastElement.insertAdjacentElement('afterbegin', this.element)
+  }
+
+}
+
+const projectElement = new ProjectInput();
